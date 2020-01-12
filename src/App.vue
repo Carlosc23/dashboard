50d8832fbe2c083301b1dcc3cf2ec4f96a7863c6
Carlosc23 <cal15219@uvg.edu.gt>
2020-01-11 22:12:11 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <section class="dashboard">
+      <header>
+        <!-- date range placeholder -->
+      </header>
+      <div class="dashboard__row">
+        <latest-transactions-chart ref="latestTransactions" />
+        <transaction-breakdown-chart ref="transactionBreakdown"  />
+      </div>
+      <div class="dashboard__row">
+        <transaction-details-grid />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import transactions from './data/transactions.js';
+
+import LatestTransactionsChart from './components/LatestTransactionsChart.vue';
+import TransactionBreakdownChart from './components/TransactionBreakdownChart.vue';
+import TransactionDetailsGrid from './components/TransactionDetailsGrid.vue';
+
+export default {
+  name: 'app',
+  components: {
+    LatestTransactionsChart,
+    TransactionBreakdownChart,
+    TransactionDetailsGrid,
+  },
+  data() {
+    return {
+      transactions,
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
---
+++ src/components/TransactionBreakdownChart.vue
@@ -0,0 +1,59 @@
+<template>
+   <zingchart :data="chartConfig"  :height="'100%'" ref="chart"/> 
+</template>
+
+<script>
+export default {
+  props: ['entries'],
+  data() {
+    return {
+    };
+  },
+  computed: {
+
+    chartConfig() {
+      const colors = [
+        {
+          backgroundColor: '#04A3F5',
+          hoverState: {
+            backgroundColor: '#45D6C4'
+          }
+        },
+        {
+          backgroundColor: '#98D1EE',
+          hoverState: {
+            backgroundColor: '#45D6C4'
+          }
+        },
+                {
+          backgroundColor: '#295A73',
+          hoverState: {
+            backgroundColor: '#45D6C4'
+          }
+        },
+      ];
+      const config ={
+        type: 'pie',
+        tooltip: {
+          text: '%npv%'
+        },
+        plotarea: {
+          margin: '5'
+        },
+        plot: {
+          valueBox: {
+            fontSize: 10,
+            text: '%t'
+          },
+          hoverState: {
+         	  borderWidth: 2,
+          }
+        },
+        // TODO: Format the data and pass it to the series.
+        series: []
+      };
+      return config;
+    },
+  }
+}
+</script>
